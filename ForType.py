5c191e28f3c6ea6939b0493d810c18ef7e3016c5
Mulualemb <elibogala@gmail.com>
2022-03-10 09:38:16 +0200

    scond commit

---
+++ ForType.py
@@ -0,0 +1,33 @@
+# #q1
+
+# for i in range(20+1):
+#     if i % 2 != 0:
+#         print(i)
+#     i = i - 1
+
+
+#q2
+# sum = 0
+# for i in range(14):
+#     sum = sum + i
+#     print(sum)
+
+
+
+#q3
+# for i in range (10+1):
+#   print("multiple by",i,'x''=',i* 1,i*2,i*3,i*4,i*5,i*6,i*7,i*8,i*9,i*10)
+
+# x = 10
+# y = 10
+# for i in range(1,y+1):
+#     for j in range(1,x+1):
+#         print(i * j,"\t",end="")
+#     print()
+
+
+
+# q4
+# for i in range (1,101):
+#     if i/i==i:
+#         print(i)
